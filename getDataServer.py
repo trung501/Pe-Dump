f3f2531ad7f41a6a2f6af2099f0a12b7826e2597
trung501 <gotrungogle@gmail.com>
2022-03-01 09:16:21 +0700

    them database

--- getDataServer.py
+++ getDataServer.py
@@ -7,16 +7,16 @@ import os
 import tempfile
 import unidecode
 
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 
 # creating fastApi app
 app = FastAPI()
 
 pathDataCsv = "PeDumpData.csv"
 
-DATABASE_URL="postgresql://tqsihnskfsieci:1d6286a180d3584430100339b1a8cd14bcb3614b195ff97267db49f2f7e79930@ec2-34-233-157-9.compute-1.amazonaws.com:5432/d35eo9b9dojb3j"
+# DATABASE_URL="postgresql://tqsihnskfsieci:1d6286a180d3584430100339b1a8cd14bcb3614b195ff97267db49f2f7e79930@ec2-34-233-157-9.compute-1.amazonaws.com:5432/d35eo9b9dojb3j"
 
-engine = create_engine( DATABASE_URL)
+# engine = create_engine( DATABASE_URL)
 
 
 
@@ -95,7 +95,7 @@ def parse(file: UploadFile = File(...), malware: bool = False):
     # else:
     #     dataframe.to_csv(pathDataCsv, mode='a', index=False, header=True)
     print(dataframe)
-    dataframe.to_sql('PeData', engine, if_exists='append', index=False)
+    # dataframe.to_sql('PeData', engine, if_exists='append', index=False)
     return dataframe.to_dict(orient="index")
     os.close(_)
     os.remove(path)
